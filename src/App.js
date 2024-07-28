6c0d2845e28b4cf7747f5876691d6d209e7728c0
whyshouldibother <prajwalshrestha2018@gmail.com>
2024-07-28 21:50:27 +0545

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+
+import "./App.css";
+
+let entities = [1, 2, 3, 4];
+
+let set = [];
+let index = 1;
+
+let rank = [];
+
+for (let i = 0; i < entities.length; i++) {
+  for (let j = i + 1; j < entities.length; j++) {
+    set = [...set, [entities[i], entities[j]]];
+  }
+}
 
 function App() {
+  const left = useRef(null);
+  const right = useRef(null);
+  const dom = useRef(null);
+  useEffect(() => {
+    left.current.innerText = set[0][0];
+    right.current.innerText = set[0][1];
+  }, []);
+
+  function next(id: boolean) {
+    rank = [...rank, set[index - 1].join(id ? "<" : ">")];
+    if (index < set.length) {
+      left.current.innerText = set[index][0];
+      right.current.innerText = set[index][1];
+      index++;
+    } else if(dom.current.children.length<3) {
+      const newChildDOMNode = dom.current.ownerDocument.createElement("div"); 
+      newChildDOMNode.className = 'message'
+      newChildDOMNode.appendChild(dom.current.ownerDocument.createTextNode("You have reached the end"))
+      dom.current.appendChild(newChildDOMNode);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={dom} className={"container"}>
+      <div
+        className="box left"
+        onClick={() => {
+          next(false);
+        }}
+        ref={left}
+      ></div>
+      <div
+        className="box right"
+        onClick={() => {
+          next(true);
+        }}
+        ref={right}
+      ></div>
     </div>
   );
 }
