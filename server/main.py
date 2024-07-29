e2a7083ebf5d40114559459c27e48ead79e5ec52
whyshouldibother <prajwalshrestha2018@gmail.com>
2024-07-30 00:32:27 +0545

    Backend

---
+++ server/main.py
@@ -0,0 +1,15 @@
+#app.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello_world():
+    return "Hello,World"
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
