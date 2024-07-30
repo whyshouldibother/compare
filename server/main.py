b06faa0ac97e3f68d2fe60cf167cfbb70efb0ed5
whyshouldibother <prajwalshrestha2018@gmail.com>
2024-07-30 20:09:48 +0545

    Removed docs end point

--- server/main.py
+++ server/main.py
@@ -5,11 +5,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import dotenv_values
 env=dotenv_values(".env")
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
 #enable cors
 origins = [env["origins"]]
 
+dev = [env["dev"]]
+if (not(dev)):
+    app = FastAPI(docs_url="/documentation", redoc_url=None)
 app.add_middleware(
     CORSMiddleware, 
     allow_origins = origins, 
@@ -18,13 +23,25 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+class post(BaseModel):
+    id: str
+    post: str
+    
+    
+@app.post("/api/post")
+def handlePost(data: post):
+    return data
 @app.get("/api/{path}")
 def handlePath(path:str):
-    return {"response": path, "origins":origins}
+    return {"response": path}
 
 @app.get("/{path}")
 def handle404(path: str):
     return path
+
+@app.get("/")
+def root():
+    return {"😎😎😎"}
 
 if __name__ == '__main__':
     import uvicorn
